370a743fc71841e401a0b84dd684d6b0a0bcf352
dorisep <ed.doris@gmail.com>
2022-01-10 12:36:18 -0600

    updated web

--- static/app.js
+++ static/app.js
@@ -2,58 +2,50 @@
 // let intiData = d3.select("#init_data")
 // buildTable(data)
 d3.json('/get_init_data').then((data) => {
-    blah = data;
-    console.log(blah);
+
+    console.log(data)
+    buildTable(data);
+    makePictures(data);
+
 });
 
 // function filterData(data) {
 //     data.forEach(element => {
-//         if (element.meta_score > 90) {
-//             console.log(element)
-//             buildTable(element)
-//         }
-//         const metaArray = Object.entries(element)
+//         buildTable(element)
 
-//         const metaScoreObject = metaArray[4]
-//             // console.log(typeof metaScoreObject[1])
-//             // metaScoreArray.filter(function([key, value]) {
-//             //     console.log(value);
-//             // });
 
 //     });
 
-//     // const filteredArr = metaArray.filter(function([album_genre, value]) {
-//     // return value > 89
-//     // })
+// const filteredArr = metaArray.filter(function([album_genre, value]) {
+// return value > 89
+// })
 // }
 
 
 function buildTable(data) {
-    console.log(data)
     d3.select("tbody")
         .selectAll("tr")
         .data(data)
         .enter()
         .append("tr")
         .html(function(d) {
-            console.log(d)
-            {
-                return `<td>${d.artist}</td>
-                <td>${d.album}</td>
-                <td>${d.album_genre}</td>
-                <td>${d.date}</td>
-                <td>${d.meta_score}</td>
-                <td>${d.user_score}</td>
-                <td>${d.record_label}</td>`
-            }
-
+            return `<td>${d[1]}</td>
+                <td>${d[2]}</td>
+                <td>${d[11]}</td>
+                <td>${d[3]}</td>
+                <td>${d[5]}</td>
+                <td>${d[6]}</td>
+                <td>${d[10]}</td>`
         });
 }
-// // d3.select("tbody")
-// // .selectAll("tr")
-// // .data(album)
-// // .enter()
-// // .append("tr")
-// // .html(function(d) {
-// //     return `<td>${d.artist}</td><td>${d.album}</td><td>${d.album_genre}</td><td>${d.date}</td><td>${d.meta_score}</td><td>${d.user_score}</td><td>${d.record_label}</td>`;
-// // });
+
+function makePictures(data) {
+    d3.select(".MagicScroll")
+        .selectAll("img")
+        .data(data)
+        .enter()
+        .append("img")
+        .html(function(d) {
+            return `<img> src=${d[7]} </img>`
+        });
+}

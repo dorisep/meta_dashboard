6e0d3bfbdd6e7e56f159ee81b01ef890c690c6f5
dorisep <ed.doris@gmail.com>
2022-01-29 09:53:24 -0600

    have graph in place
    didn't catch that x axis values was two arrays instead of one

--- static/app.js
+++ static/app.js
@@ -1,7 +1,8 @@
 d3.json('/get_init_data').then((data) => {
 
     buildTable(data);
-    buildLineChart(data);
+    // buildLineChart(data);
+    graph(traceAbove90(data));
 });
 
 
@@ -26,8 +27,7 @@ function traceAbove90(data, threshold = 90) {
     let years = []
     let scores = {}
     var trace = {
-        x: [],
-        y: [],
+
         mode: 'lines+markers',
         type: 'scatter'
     };
@@ -38,33 +38,153 @@ function traceAbove90(data, threshold = 90) {
         }
     });
     // console.log(Object.values(scores))
-    trace["x"].push(Object.keys(scores))
-    trace["y"].push(Object.values(scores))
+    trace["x"] = [Object.keys(scores)];
+    trace["y"] = [Object.values(scores)];
         // console.log(trace["x"])
         // console.log((trace["y"]))
 
     return trace
 }
 
-function buildLineChart(data) {
-
-    data = traceAbove90(data);
-    console.log(data)
-    var layout = {
-        title: 'Metacritic Scores Over Time',
-
+function graph (trace1) {
+    
+    var xData = trace1.x;
+      
+      var yData = trace1.y;
+      
+      var colors = ['rgba(67,67,67,1)', 'rgba(115,115,115,1)', 'rgba(49,130,189, 1)',
+        'rgba(189,189,189,1)'
+      ];
+      
+      var lineSize = [2, 2, 4, 2];
+      
+      var labels = ['Television', 'Newspaper', 'Internet', 'Radio'];
+      
+      var data = [];
+      
+      for ( var i = 0 ; i < xData.length ; i++ ) {
+        var result = {
+          x: xData[i],
+          y: yData[i],
+          type: 'scatter',
+          mode: 'lines',
+          line: {
+            color: colors[i],
+            width: lineSize[i]
+          }
+        };
+        var result2 = {
+          x: [xData[i][0], xData[i][11]],
+          y: [yData[i][0], yData[i][11]],
+          type: 'scatter',
+          mode: 'markers',
+          marker: {
+            color: colors[i],
+            size: 12
+          }
+        };
+        data.push(result, result2);
+      }
+      
+      var layout = {
+        // showlegend: false,
+        // height: 600,
+        // width: 600,
         xaxis: {
-            showline: true,
-            showticklabels: true,
-            autotick: false
+          showline: true,
+          showgrid: false,
+          showticklabels: true,
+          linecolor: 'rgb(204,204,204)',
+          linewidth: 2,
+          autotick: false,
+          ticks: 'outside',
+          tickcolor: 'rgb(204,204,204)',
+          tickwidth: 2,
+          ticklen: 5,
+          tickfont: {
+            family: 'Arial',
+            size: 12,
+            color: 'rgb(82, 82, 82)'
+          }
         },
         yaxis: {
-            showgrid: false,
-            zeroline: false,
-            showline: false,
-            showticklabels: false
-        }
-    };
-
-    Plotly.newPlot('myDiv', data, layout);
+          showgrid: false,
+          zeroline: false,
+          showline: false,
+          showticklabels: false
+        },
+        autosize: false,
+        margin: {
+          autoexpand: false,
+          l: 100,
+          r: 20,
+          t: 100
+        },
+        annotations: [
+          {
+            xref: 'paper',
+            yref: 'paper',
+            x: 0.0,
+            y: 1.05,
+            xanchor: 'left',
+            yanchor: 'bottom',
+            text: 'Main Source for News',
+            font:{
+              family: 'Arial',
+              size: 30,
+              color: 'rgb(37,37,37)'
+            },
+            showarrow: false
+          },
+          {
+            xref: 'paper',
+            yref: 'paper',
+            x: 0.5,
+            y: -0.1,
+            xanchor: 'center',
+            yanchor: 'top',
+            showarrow: false,
+            font: {
+              family: 'Arial',
+              size: 12,
+              color: 'rgb(150,150,150)'
+            }
+          }
+        ]
+      };
+      
+      for( var i = 0 ; i < xData.length ; i++ ) {
+        var result = {
+          xref: 'paper',
+          x: 0.05,
+          y: yData[i][0],
+          xanchor: 'right',
+          yanchor: 'middle',
+          text: labels[i] + ' ' + yData[i][0] +'%',
+          showarrow: false,
+          font: {
+            family: 'Arial',
+            size: 16,
+            color: 'black'
+          }
+        };
+        var result2 = {
+          xref: 'paper',
+          x: 0.95,
+          y: yData[i][11],
+          xanchor: 'left',
+          yanchor: 'middle',
+          text: yData[i][11] +'%',
+          font: {
+            family: 'Arial',
+            size: 16,
+            color: 'black'
+          },
+          showarrow: false
+        };
+      
+        layout.annotations.push(result, result2);
+      }
+      
+      Plotly.newPlot('myDiv', data, layout);
 }

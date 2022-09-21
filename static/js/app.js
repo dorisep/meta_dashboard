b79d42feeb4594d94a2ac61cddc2ba38d3090cb3
dorisep <ed.doris@gmail.com>
2022-09-21 14:35:29 -0500

    parsed data array for bar chart
    created object for count of albums by year using reduce.

--- static/js/app.js
+++ static/js/app.js
@@ -2,16 +2,15 @@ let musicData
 
 d3.json("/get_init_data", function(data) {
         musicData = data;
-        // drawBarChart(data);
         buildTable(data);
+        drawBarChart(data);
+
 
     })
     // start of code for table
 var tbody = d3.select("tbody");
 
 function buildTable(tData) {
-    console.log('----buildTable called----')
-    console.log(tData)
     tbody.html("");
     tData.forEach(record => {
         let textArray = [record.date, record.album, record.artist, record.album_genre, record.record_label, record.meta_score];
@@ -51,78 +50,83 @@ d3.selectAll("input").on("change", updateFilters);
 
 // barchart link:https://d3-graph-gallery.com/graph/barplot_stacked_basicWide.html
 // set the dimensions and margins of the graph
-// var margin = {top: 10, right: 30, bottom: 20, left: 50},
-//     width = 460 - margin.left - margin.right,
-//     height = 400 - margin.top - margin.bottom;
+var margin = { top: 10, right: 30, bottom: 20, left: 50 },
+    width = 460 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
 
-// // append the svg object to the body of the page
-// var svg = d3.select("#my_dataviz_bar")
-// .append("svg")
-//     .attr("width", width + margin.left + margin.right)
-//     .attr("height", height + margin.top + margin.bottom)
-// .append("g")
-//     .attr("transform",
-//         "translate(" + margin.left + "," + margin.top + ")");
+// append the svg object to the body of the page
+var svg = d3.select("#my_dataviz_bar")
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform",
+        "translate(" + margin.left + "," + margin.top + ")");
 
-// let csvData
-// d3.csv("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/data_stacked.csv", function(d) {
-//     csvData = d
-// });
-// console.log("----csv data call----")
-// console.log(csvData)    
+function drawBarChart(bData) {
+    console.log('----drawBarChart called----')
+    let countByDates = bData.reduce((resultSet, item) => ({
+        ...resultSet,
+        [item.date]: (resultSet[item.date] || 0) + 1,
+    }), {});
 
-// function drawBarChart(bData) { 
-//     console.log('----drawBarChart called----') 
-//     console.log(bData);
-// Parse the Data
+    console.log(countByDates);
 
-// // List of subgroups = header of the csv files = soil condition here
-// var subgroups = data.columns.slice(1)
+    // let textArray = [record.date, record.album, record.artist, record.album_genre, record.record_label, record.meta_score];
 
-// // List of groups = species here = value of the first column called group -> I show them on the X axis
-// var groups = d3.map(data, function(d){return(d.group)}).keys()
+    // textArray.forEach(val => {
+    //     cell = row.append("td");
+    //     cell.text(val);
+    // });
+};
+console.log(dateArray)
+    //     // Parse the Data
 
-// // Add X axis
-// var x = d3.scaleBand()
-//     .domain(groups)
-//     .range([0, width])
-//     .padding([0.2])
-// svg.append("g")
-//     .attr("transform", "translate(0," + height + ")")
-//     .call(d3.axisBottom(x).tickSizeOuter(0));
+//     // List of subgroups = header of the csv files = soil condition here
+//     var subgroups = data.columns.slice(1)
 
-// // Add Y axis
-// var y = d3.scaleLinear()
-//     .domain([0, 60])
-//     .range([ height, 0 ]);
-// svg.append("g")
-//     .call(d3.axisLeft(y));
+//     // List of groups = species here = value of the first column called group -> I show them on the X axis
+//     var groups = d3.map(data, function(d) { return (d.group) }).keys()
 
-// // color palette = one color per subgroup
-// var color = d3.scaleOrdinal()
-//     .domain(subgroups)
-//     .range(['#e41a1c','#377eb8','#4daf4a'])
+//     // Add X axis
+//     var x = d3.scaleBand()
+//         .domain(groups)
+//         .range([0, width])
+//         .padding([0.2])
+//     svg.append("g")
+//         .attr("transform", "translate(0," + height + ")")
+//         .call(d3.axisBottom(x).tickSizeOuter(0));
 
-// //stack the data? --> stack per subgroup
-// var stackedData = d3.stack()
-//     .keys(subgroups)
-//     (data)
+//     // Add Y axis
+//     var y = d3.scaleLinear()
+//         .domain([0, 60])
+//         .range([height, 0]);
+//     svg.append("g")
+//         .call(d3.axisLeft(y));
 
-// // Show the bars
-// svg.append("g")
-//     .selectAll("g")
-//     // Enter in the stack data = loop key per key = group per group
-//     .data(stackedData)
-//     .enter().append("g")
-//     .attr("fill", function(d) { return color(d.key); })
-//     .selectAll("rect")
-//     // enter a second time = loop subgroup per subgroup to add all rectangles
-//     .data(function(d) { return d; })
-//     .enter().append("rect")
+//     // color palette = one color per subgroup
+//     var color = d3.scaleOrdinal()
+//         .domain(subgroups)
+//         .range(['#e41a1c', '#377eb8', '#4daf4a'])
+
+//     //stack the data? --> stack per subgroup
+//     var stackedData = d3.stack()
+//         .keys(subgroups)
+//         (data)
+
+//     // Show the bars
+//     svg.append("g")
+//         .selectAll("g")
+//         // Enter in the stack data = loop key per key = group per group
+//         .data(stackedData)
+//         .enter().append("g")
+//         .attr("fill", function(d) { return color(d.key); })
+//         .selectAll("rect")
+//         // enter a second time = loop subgroup per subgroup to add all rectangles
+//         .data(function(d) { return d; })
+//         .enter().append("rect")
 //         .attr("x", function(d) { return x(d.data.group); })
 //         .attr("y", function(d) { return y(d[1]); })
 //         .attr("height", function(d) { return y(d[0]) - y(d[1]); })
-//         .attr("width",x.bandwidth())
-// })
-// }
-// drawBarChart(musicData)
+//         .attr("width", x.bandwidth())
+//         // })

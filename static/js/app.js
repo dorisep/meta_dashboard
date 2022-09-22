3556a60d9e54d7ea8cb23ba0ec460695ecc42ef4
dorisep <ed.doris@gmail.com>
2022-09-22 14:55:55 -0500

    x and y axis render

--- static/js/app.js
+++ static/js/app.js
@@ -98,80 +98,61 @@ function parseDataBar(data){
             binScoresByYear[currentYear][bin] += 1
         }        
     }
+    return binScoresByYear
 }
-
+let chartDataObj
+let subgroups
 function drawBarChart(bData) {
     console.log('----drawBarChart called----')
-    parseDataBar(bData)
-    // for (const [key, value] of Object.entries(binScoresByYear)) {
-    //     console.log(`${key}: ${value}`);
-    //   }
-    // let sortedScores = bData.filter((record)=>{
-    //     console.log(record.meta_score)
-    // })
-    // let countByDates = bData.reduce((resultSet, item) => ({
-    //     ...resultSet,
-    //     [item.date]: (resultSet[item.date] || 0) + 1,
-    // }), {});
+    chartDataObj = parseDataBar(bData)
+    groups = Object.keys(chartDataObj)
+    // for x
+    subgroups = Object.keys(chartDataObj[groups[0]])
+    // for y
+    // for stack
 
 
-    // let textArray = [record.date, record.album, record.artist, record.album_genre, record.record_label, record.meta_score];
+  // Add X axis
+  var x = d3.scaleBand()
+      .domain(groups)
+      .range([0, width])
+      .padding([0.2])
+  svg.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x).tickSizeOuter(0));
 
-    // textArray.forEach(val => {
-    //     cell = row.append("td");
-    //     cell.text(val);
-    // });
-};
-// console.log(dateArray)
-    //     // Parse the Data
+  // Add Y axis
+  var y = d3.scaleLinear()
+    .domain([0, 60])
+    .range([ height, 0 ]);
+  svg.append("g")
+    .call(d3.axisLeft(y));
 
-//     // List of subgroups = header of the csv files = soil condition here
-//     var subgroups = data.columns.slice(1)
+  // color palette = one color per subgroup
+  var color = d3.scaleOrdinal()
+    .domain(subgroups)
+    .range(['#e41a1c','#377eb8','#4daf4a'])
 
-//     // List of groups = species here = value of the first column called group -> I show them on the X axis
-//     var groups = d3.map(data, function(d) { return (d.group) }).keys()
+  //stack the data? --> stack per subgroup
+  var stackedData = d3.stack()
+    .keys(subgroups)
+    (data)
 
-//     // Add X axis
-//     var x = d3.scaleBand()
-//         .domain(groups)
-//         .range([0, width])
-//         .padding([0.2])
-//     svg.append("g")
-//         .attr("transform", "translate(0," + height + ")")
-//         .call(d3.axisBottom(x).tickSizeOuter(0));
+  // Show the bars
+  svg.append("g")
+    .selectAll("g")
+    // Enter in the stack data = loop key per key = group per group
+    .data(stackedData)
+    .enter().append("g")
+      .attr("fill", function(d) { return color(d.key); })
+      .selectAll("rect")
+      // enter a second time = loop subgroup per subgroup to add all rectangles
+      .data(function(d) { return d; })
+      .enter().append("rect")
+        .attr("x", function(d) { return x(d.data.group); })
+        .attr("y", function(d) { return y(d[1]); })
+        .attr("height", function(d) { return y(d[0]) - y(d[1]); })
+        .attr("width",x.bandwidth())
+}
 
-//     // Add Y axis
-//     var y = d3.scaleLinear()
-//         .domain([0, 60])
-//         .range([height, 0]);
-//     svg.append("g")
-//         .call(d3.axisLeft(y));
-
-//     // color palette = one color per subgroup
-//     var color = d3.scaleOrdinal()
-//         .domain(subgroups)
-//         .range(['#e41a1c', '#377eb8', '#4daf4a'])
-
-//     //stack the data? --> stack per subgroup
-//     var stackedData = d3.stack()
-//         .keys(subgroups)
-//         (data)
-
-//     // Show the bars
-//     svg.append("g")
-//         .selectAll("g")
-//         // Enter in the stack data = loop key per key = group per group
-//         .data(stackedData)
-//         .enter().append("g")
-//         .attr("fill", function(d) { return color(d.key); })
-//         .selectAll("rect")
-//         // enter a second time = loop subgroup per subgroup to add all rectangles
-//         .data(function(d) { return d; })
-//         .enter().append("rect")
-//         .attr("x", function(d) { return x(d.data.group); })
-//         .attr("y", function(d) { return y(d[1]); })
-//         .attr("height", function(d) { return y(d[0]) - y(d[1]); })
-//         .attr("width", x.bandwidth())
-//         // })
-// event listeners
 d3.selectAll("input").on("change", updateFilters);

ef9b30107c58b89c8821a9576e1828d36c49988e
dorisep <ed.doris@gmail.com>
2022-09-26 21:30:30 -0500

    got bars added to chart

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,21 @@
 let musicData
 
+// LINE 164 
+// d3.csv("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/data_stacked.csv", function(data) {
+// CHECK THE RETURNS OF EACH LINE
+//   .enter().append("rect")
+// .attr("x", function(d) { return x(d.data.group); })
+// .attr("y", function(d) { return y(d[1]); })
+// .attr("height", function(d) { return y(d[0]) - y(d[1]); })
+// .attr("width",x.bandwidth())
 
+
+// subgroups = ['Nitrogen', 'normal', 'stress']
+// groups =  ['banana', 'poacee', 'sorgho', 'triticum']
+// stackedData = 0
+// [[0, 12], [0, 6], [0, 11], [0, 19], key: 'Nitrogen', index: 0]
+// [[12,13], [6, 12], [11, 39], [19, 25], key: 'normal', index: 1]
+// [[13, 26], [12, 45], [39, 51], [25, 26], key: 'stress', index: 2]
 d3.json("/get_init_data", function(data) {
         musicData = data;
         buildTable(data);
@@ -69,100 +84,108 @@ function filterScores(score) {
     // filters scores in ranges
     console.log('-----filterScores------')
 
-    if(score >= 90) {
+    if (score >= 90) {
         bin = "90s"
         return bin
-    }else if(score<90 && score>=80){
+    } else if (score < 90 && score >= 80) {
         return "80s"
-    }else if(score<80 && score>=70){
+    } else if (score < 80 && score >= 70) {
         return "70s"
-    }else{
+    } else {
         return "60s"
     }
 }
 let binScoresByYear
-function parseDataBar(data){
+
+function parseDataBar(data) {
     // parse data into an object containing years and range of scores as keys with counts as values
     binScoresByYear = {};
-    let count = 0;
-    for (i=0; i < data.length; i++){
+    for (i = 0; i < data.length; i++) {
         let currentYear = data[i].date
         let bin = filterScores(data[i].meta_score)
         if (typeof binScoresByYear[currentYear] === 'undefined') {
             binScoresByYear[currentYear] = {};
-            binScoresByYear[currentYear]['year'] = currentYear;          
+            binScoresByYear[currentYear]['year'] = currentYear;
             binScoresByYear[currentYear][bin] = 1;
-        }else if(typeof binScoresByYear[currentYear][bin] === 'undefined'){
+        } else if (typeof binScoresByYear[currentYear][bin] === 'undefined') {
             binScoresByYear[currentYear][bin] = 1;
-        }else{
+        } else {
             binScoresByYear[currentYear][bin] += 1;
-        }        
+        }
+
     }
     return binScoresByYear
 }
 let chartDataObj
 let subgroups
 let groups
-let barArray = []
+let stackedData
+
+function makeBarArray(obj) {
+    let anArray = []
+    for (const key of Object.keys(chartDataObj)) {
+        anArray.push(chartDataObj[`${key}`])
+    }
+    return anArray
+}
+
+function fillInMissingSubgroups() {
+
+}
+let meh
+let myStack
+
 function drawBarChart(bData) {
     console.log('----drawBarChart called----')
     chartDataObj = parseDataBar(bData)
+    meh = makeBarArray(chartDataObj)
 
-    for (const key of Object.keys(chartDataObj)) {
-        barArray.push(chartDataObj[`${key}`])
-      }
 
     groups = Object.keys(chartDataObj)
-    // for x
-    subgroups = Object.keys(chartDataObj[groups[0]])
-    // for y
-    // for stack
+        // for x
+    subgroups = (Object.keys(chartDataObj[groups[0]]).slice(1))
+        // Add X axis
+    var x = d3.scaleBand()
+        .domain(groups)
+        .range([0, width])
+        .padding([0.2])
+    svg.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x).tickSizeOuter(0));
 
-    for (const key of Object.keys(chartDataObj)) {
-        a.push(chartDataObj[`${key}`])
-      }
-//   // Add X axis
-//   var x = d3.scaleBand()
-//       .domain(groups)
-//       .range([0, width])
-//       .padding([0.2])
-//   svg.append("g")
-//     .attr("transform", "translate(0," + height + ")")
-//     .call(d3.axisBottom(x).tickSizeOuter(0));
+    // Add Y axis
+    var y = d3.scaleLinear()
+        .domain([0, 60])
+        .range([height, 0]);
+    svg.append("g")
+        .call(d3.axisLeft(y));
 
-//   // Add Y axis
-//   var y = d3.scaleLinear()
-//     .domain([0, 60])
-//     .range([ height, 0 ]);
-//   svg.append("g")
-//     .call(d3.axisLeft(y));
+    // color palette = one color per subgroup
+    var color = d3.scaleOrdinal()
+        .domain(subgroups)
+        .range(['#F43108 ', '#FED4CB ', '#9E9B9A', '#000000'])
 
-//   // color palette = one color per subgroup
-//   var color = d3.scaleOrdinal()
-//     .domain(subgroups)
-//     .range(['#e41a1c','#377eb8','#4daf4a'])
-
-//   //stack the data? --> stack per subgroup
-//   var stackedData = d3.stack()
-//     .keys(subgroups)
-//     (data)
-
-//   // Show the bars
-//   svg.append("g")
-//     .selectAll("g")
-//     // Enter in the stack data = loop key per key = group per group
-//     .data(stackedData)
-//     .enter().append("g")
-//       .attr("fill", function(d) { return color(d.key); })
-//       .selectAll("rect")
-//       // enter a second time = loop subgroup per subgroup to add all rectangles
-//       .data(function(d) { return d; })
-//       .enter().append("rect")
-//         .attr("x", function(d) { return x(d.data.group); })
-//         .attr("y", function(d) { return y(d[1]); })
-//         .attr("height", function(d) { return y(d[0]) - y(d[1]); })
-//         .attr("width",x.bandwidth())
+    //stack the data? --> stack per subgroup
+    stackedData = d3.stack()
+        .keys(subgroups)
+        (meh)
+    myStack = stackedData
+        // Show the bars
+    svg.append("g")
+        .selectAll("g")
+        // Enter in the stack data = loop key per key = group per group
+        .data(stackedData)
+        .enter().append("g")
+        .attr("fill", function(d) { return color(d.key); })
+        .selectAll("rect")
+        // enter a second time = loop subgroup per subgroup to add all rectangles
+        .data(function(d) { return d; })
+        .enter().append("rect")
+        .attr("x", function(d) { return x(d.data.year); })
+        .attr("y", function(d) { return y(d[1]); })
+        .attr("height", function(d) { return y(d[0]) - y(d[1]); })
+        .attr("width", x.bandwidth())
 }
 
-d3.selectAll("input").on("change", updateFilters);
 
+d3.selectAll("input").on("change", updateFilters);
